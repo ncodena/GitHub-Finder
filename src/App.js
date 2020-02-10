13f97b533c7903a876ddaeb2aff5b8100929df51
Nuria <c.odena.nuria@gmail.com>
2020-02-10 17:39:57 +0100

    react router setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, {Fragment, Component} from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Navbar from './layouts/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
 import Alert from './layouts/Alert';
+import About from './components/pages/About';
 import axios from 'axios';
 import './App.css';
 
-class App extends React.Component {
+class App extends Component {
 
   state = {
     users: [],
@@ -44,21 +46,27 @@ class App extends React.Component {
 
     const {users, loading, alert} = this.state;
   return (
+    <Router>
       <div className="App">
         <Navbar/>
         <div className="container">
           <Alert alert={alert} />
-          <Search 
-          searchUsers={this.searchUsers} 
-          clearUsers={this.clearUsers} 
-          showClear={users.length > 0 ? true: false}
-          setAlert={this.setAlert} />
-          <Users 
-          loading={loading} 
-          users={users}/>
+          <Switch>
+            <Route exact path= '/' render={props =>
+              <Fragment>
+                <Search 
+                searchUsers={this.searchUsers} 
+                clearUsers={this.clearUsers} 
+                showClear={users.length > 0 ? true: false}
+                setAlert={this.setAlert} />
+                <Users loading={loading} users={users}/>
+              </Fragment>
+              }/>
+              <Route exact path = '/about' component={About}/>
+          </Switch>
         </div>
-        
       </div>
+    </Router>
     );
   }
 }
